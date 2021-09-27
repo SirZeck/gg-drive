a24b5fc85fb86ac6fc12b909c046ab3616761329
Aditya <adithebest.ar@gmail.com>
2021-09-27 22:24:39 +0530

    initialized project

---
+++ .history/src/App_20210927221533.js
@@ -0,0 +1,8 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return "hello world"
+}
+
+export default App;
---
+++ .history/src/components/App_20210927221810.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App() {
+  return "hello world"
+}
+
+export default App;
