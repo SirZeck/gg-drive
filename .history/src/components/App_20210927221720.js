8c164ff4d9fab61e27f24e409ca49440259b0c06
Aditya <adithebest.ar@gmail.com>
2021-09-28 19:08:48 +0530

    updated gitignore

--- .history/src/components/App_20210927221720.js
+++
@@ -1,8 +0,0 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return "hello world"
-}
-
-export default App;
--- .history/src/components/App_20210928134825.js
+++
@@ -1,9 +0,0 @@
-//import React from "react";
-
-import Signup from "./Signup";
-
-function App() {
-  return <Signup />
-}
-
-export default App;
